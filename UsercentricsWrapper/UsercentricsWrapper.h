81ca0893578a3be8b051e564ee83ea727c7be9a8
Gevorg Kopalyan <gevorg.kopalyan@gmail.com>
2024-12-03 16:21:54 +0400

    Initial Commit

---
+++ UsercentricsWrapper/UsercentricsWrapper.h
@@ -0,0 +1,18 @@
+//
+//  UsercentricsWrapper.h
+//  UsercentricsWrapper
+//
+//  Created by Gevorg Kopalyan on 03.12.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UsercentricsWrapper.
+FOUNDATION_EXPORT double UsercentricsWrapperVersionNumber;
+
+//! Project version string for UsercentricsWrapper.
+FOUNDATION_EXPORT const unsigned char UsercentricsWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UsercentricsWrapper/PublicHeader.h>
+
+
