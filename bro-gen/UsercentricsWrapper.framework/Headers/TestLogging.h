375b17e18a6a4365e6b921f5155c3d8ba9e13995
Gevorg Kopalyan <gevorg.kopalyan@gmail.com>
2024-12-12 16:27:32 +0400

    Bro-gen
    Bro-gen for generating bindings

---
+++ bro-gen/UsercentricsWrapper.framework/Headers/TestLogging.h
@@ -0,0 +1,18 @@
+//
+//  TestLogging.h
+//  HellowWorld
+//
+//  Created by Gevorg Kopalyan on 29.11.24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestLogging : NSObject
+
+- (void)sayHello;
+
+@end
+
+NS_ASSUME_NONNULL_END
